13d02b4de3012300d6c04bfc588eb851ffe51f4d
Raymond <raymondhvh@hotmail.com>
2016-09-22 14:12:26 +0200

    models made

---
+++ asp.net/WebApplication1/Models/SchoolGroup.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Smoelenboek.Models
+{
+    public class SchoolGroup
+    {
+        public int ID { get; set; }
+        public string GroupName { get; set; }
+    }
+}
---
+++ asp.net/WebApplication1/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Smoelenboek.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+        public string SchoolGroup { get; set; }
+
+     //   public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
